00dc36cf5f818fbfcf017e9d85c7b8aafda2d802
ferschine <141339296+ferschine@users.noreply.github.com>
2025-06-27 07:37:45 -0300

    .

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
 const numeros = '0123456789';
 const simbolos = '!@%*?';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
-
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
